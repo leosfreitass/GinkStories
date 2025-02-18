cd331dbd2c1d863713b3d356a89c8a8e361cb4d9
Hinkami <herivelton.pro@gmail.com>
2025-02-18 17:35:37 -0300

    login e register criado + html home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
   }
 ];
