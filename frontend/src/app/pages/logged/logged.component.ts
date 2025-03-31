5fef096930965c5b2d99f7c11f4a88a35ba51958
Hugo <hugomonteiroguedes@gmail.com>
2025-03-31 14:28:55 -0300

    logged page created

---
+++ frontend/src/app/pages/logged/logged.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-logged',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './logged.component.html',
+  styleUrl: './logged.component.scss'
+})
+export class LoggedComponent {
+
+}
