2e43dc804dd69ba6f5253c46b03bcf4daff2c34d
Hinkami <herivelton.pro@gmail.com>
2025-04-01 18:02:47 -0300

    CSS FIXED: Home when no feed message

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../components/header/header.component";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
