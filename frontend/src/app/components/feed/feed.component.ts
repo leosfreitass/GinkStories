e45da7cff0d5542122137533812f65aefcc8dab4
Hinkami <herivelton.pro@gmail.com>
2025-04-02 17:14:35 -0300

    feed component css

--- frontend/src/app/components/feed/feed.component.ts
+++ frontend/src/app/components/feed/feed.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-feed',
   imports: [
-    NgOptimizedImage
   ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss'
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../components/header/header.component";
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {FeedComponent} from "../../components/feed/feed.component";
 
 @Component({
   selector: 'app-home',
   imports: [
     HeaderComponent,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    FeedComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
