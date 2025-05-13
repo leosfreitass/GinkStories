1e5585513d484d89ef4518219d488770079fa81b
Hugo <hugomonteiroguedes@gmail.com>
2025-05-13 16:04:25 -0300

    dinamic header done

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -13,7 +13,20 @@ import {NgIf} from "@angular/common";
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
+  currentUrl: string = '';
+  buttonVisibility: { [key: string]: string[] } = {
+    login: ['/signup','/','/index'],
+    signup: ['/login','/','/index'],
+
+  }
   constructor(public router: Router) {
+    this.router.events.subscribe(() =>{
+      this.currentUrl = this.router.url;
+    })
+  }
+
+  shouldShow(button: string) : boolean {
+    return this.buttonVisibility[button]?.includes(this.currentUrl);
   }
 
   ngOnInit() {
